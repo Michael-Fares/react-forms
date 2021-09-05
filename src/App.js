d20c9408577fa732175cebe3b650080cc71659e4
Michael-Fares <mjfares@gmail.com>
2021-09-05 11:58:26 -0500

    react form posts to server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import axios from 'axios';
+
+
 import './App.css';
 
-function App() {
+
+
+
+class App extends Component {
+    constructor(props) {
+      super(props)
+
+      this.state = {
+        firstName: "",
+        lastName: ""
+      }
+    }
+  
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value 
+    })
+  }
+
+
+  handleSubmit = (e) => {
+    console.log(e)
+
+    e.preventDefault()
+
+
+    // use axios to post to server from 311_wk4_day2
+    // posts just a new user fname and lname to users table
+    // got it to work in firefox turning off CORS
+    // but not in Chrome
+    axios.post('http://localhost:4001/users', {
+      first_name: this.state.firstName,
+      last_name: this.state.lastName
+    })
+    .then(function (response) {
+      console.log('response', response);
+    })
+    .catch(function (error) {
+      console.log('THERE WAS AN ERROR', error);
+    });
+
+    // reset the form
+    this.setState( {
+      firstName: "",
+      lastName: ""
+    } )
+  }
+
+  render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sample React Form</h1>
+      <form>
+        <input name="firstName" type="text" value={this.state.firstName} onChange={(e) => this.handleChange(e)}></input>
+        <input name="lastName" type="text" value={this.state.lastName} onChange={(e) => this.handleChange(e)}></input>
+        <input type="submit" onClick = {(e) => {this.handleSubmit(e)}}></input>
+      </form>
     </div>
   );
+  }
 }
 
 export default App;
